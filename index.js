f9678bc0a1a90468e70f030f280b1013b4b987af
Medini Bochra <bochramedini@yahoo.com>
2022-08-13 21:32:05 +0100

    first github Jenkins project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const parkings = require('./parkings.json')
+
+app.get('/parkings', (req,res) => {
+    res.status(200).json(parkings)
+})
+
+app.get('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const parking = parkings.find(parking => parking.id === id)
+    res.status(200).json(parking)
+})
+
+app.listen(8050, () => {
+    console.log("Serveur à l'écoute")
+})
